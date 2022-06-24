6097df5682582288f019aff5cc3d054d1510ff44
Витя Кулеш <60291267+VitiaCatDragon@users.noreply.github.com>
2022-06-24 18:07:48 +0300

    Обноружение серверов, фиксы багов

--- rcc/rcc_client.lua
+++ rcc/rcc_client.lua
@@ -8,16 +8,15 @@ local thread = require('thread')
 local sha256 = require('sha256')
 
 local args, options = shell.parse(...)
+local port = tonumber(options.port) or 369
 
 if options.help then
-    print('rcc_client [--debug]')
-    print('rcc_client upload [file path]')
+    print('rcc_client [--debug] [--port=369]\nrcc_client upload [file path]')
     os.exit()
 end
 
-print('Remote Computer Control Client v1 [by DesConnet and Vitiacat]\nConnecting...')
-
-modem.open(369)
+print('Remote Computer Control Client v1 [by DesConnet and Vitiacat]\nFinding servers...')
+modem.open(port)
 
 local server_answered = true
 local time = 0
@@ -26,9 +25,10 @@ local is_logged = false
 local readyToSendFile = false
 local continueSendFile = true
 local current_directory = '/home'
-local text = ''
 local continue = true
 local latest_msg_id = nil
+local server_address = ''
+local servers = {}
 
 local function split(s, delimiter)
     local result = {};
@@ -43,13 +43,15 @@ local function onMessage(_, _, from, port, _, message)
     local uid = tonumber(sp[1])
     local id = sp[2]
 
-    if latest_msg_id == nil then
-        latest_msg_id = uid
-    else
-        if latest_msg_id >= uid then
-            do return end
+    if is_logged then
+        if latest_msg_id == nil then
+            latest_msg_id = uid
+        else
+            if latest_msg_id >= uid then
+                do return end
+            end
+            latest_msg_id = uid
         end
-        latest_msg_id = uid
     end
 
     if options.debug then
@@ -63,11 +65,11 @@ local function onMessage(_, _, from, port, _, message)
 
     -- id = 1 - ping, 2 - command answer, 3 - rcc answers, 4 - error --
     if id == '2' then
+        modem.send(server_address, port, 'rcc getCurrentDirectory')
         continue = false
         server_answered = true
         os.sleep(0.3)
         if text ~= '' then print(text) end
-        modem.broadcast(369, 'rcc getCurrentDirectory')
         continue = true
     elseif id == '3' then
         local arr = split(text, ' ')
@@ -85,9 +87,18 @@ local function onMessage(_, _, from, port, _, message)
             if arr[2] == 'ok' then
                 is_logged = true
             else
-                io.stderr:write('Login failed')
+                io.stderr:write('Login failed\n')
                 is_logged = 2
             end
+        elseif arr[1] == 'info' then
+            local contains = false
+            for _, server in ipairs(servers) do
+                if server.address == arr[3] then
+                    contains = true
+                    break
+                end
+            end
+            if not contains then table.insert(servers, {name = arr[2], address = arr[3]}) end
         end
     elseif id == '4' then
         io.stderr:write(text .. '\n')
@@ -103,13 +114,52 @@ end
 
 event.listen('modem_message', onMessage)
 
+modem.broadcast(port, 'rcc info')
+
+while time < 2.5 do
+    time = time + 0.1
+    os.sleep(0.1)
+end
+
+local text = ''
+
+if #servers == 0 then
+    print('No servers found ;-;')
+    return
+end
+
+if #servers ~= 1 then
+
+    for i, server in ipairs(servers) do
+        text = string.format('%s[ %s ] %s - %s\n', text, i, server.name, server.address)
+    end
+
+    print(text)
+    io.write('Select server: ')
+    local select = io.read()
+    select = tonumber(select)
+    if not select then
+        print('Incorrect input')
+        os.exit()
+    end
+    if #servers < select or select < 1 then
+        print('Incorrect input')
+        os.exit()
+    end
+    server_address = servers[select].address
+else
+    server_address = servers[1].address
+end
+
+print('Connecting...')
+
 -- check if server is live
-modem.broadcast(369, 'rcc ping')
+modem.send(server_address, port, 'rcc ping')
 while not is_pinged do
     time = time + 0.1
-    modem.broadcast(369, 'rcc ping')
+    modem.send(server_address, port, 'rcc ping')
     if time > 5 then
-        io.stderr:write('Can\'t connect to server.')
+        io.stderr:write('Can\'t connect to server.\n')
         os.exit()
     end
     os.sleep(0.1)
@@ -122,7 +172,7 @@ io.write('Username: ')
 local username = io.read()
 io.write('Password: ')
 local password = io.read()
-modem.broadcast(369, 'rcc login ' .. username .. ' ' .. sha256(password))
+modem.send(server_address, port, 'rcc login ' .. username .. ' ' .. sha256(password))
 while not is_logged do
     time = time + 0.1
     if time > 5 then
@@ -162,7 +212,7 @@ local function sendFile(path)
     local size = fs.size(path)
     local read = 0
     local _, y = term.getCursor()
-    modem.broadcast(369, 'rcc sendFile ' ..  path)
+    modem.send(server_address, port, 'rcc sendFile ' ..  path)
     while(not readyToSendFile) do
         os.sleep(0.1)
     end
@@ -172,7 +222,7 @@ local function sendFile(path)
     local t = thread.create(function ()
         term.setCursor(1, y + 1)
         term.write('Press Ctrl + C to cancel upload')
-        local value = event.pull('interrupt')
+        local value = event.pull('interrupted')
         cancelled = true
         term.setCursor(1, y + 1)
         term.clearLine()
@@ -198,7 +248,7 @@ local function sendFile(path)
                 break
             end
             os.sleep(0.05)
-            modem.broadcast(369, bytes)
+            modem.send(server_address, port, bytes)
             time = time + 0.05
         end
         if not cancelled then
@@ -209,7 +259,7 @@ local function sendFile(path)
     end
     if cancelled then
         os.sleep(1)
-        modem.broadcast(369, 'rcc cancelSendFile')
+        modem.send(server_address, port, 'rcc cancelSendFile')
         term.setCursor(1, y + 2)
         if time >= 2.5 then
             print('File sent timeout')
@@ -222,12 +272,17 @@ local function sendFile(path)
     t2:kill()
     term.setCursor(1, y + 2)
     os.sleep(1)
-    modem.broadcast(369, 'rcc endSendFile ')
+    modem.send(server_address, port, 'rcc endSendFile')
     print('File has been sent.')
     os.exit()
 end
 
 local function listenCommands()
+    event.listen('interrupted', function ()
+        text = 'exit'
+        os.exit()
+    end)
+    print('Press Ctrl + C or type exit to exit.')
     while (text ~= 'exit') do
         if server_answered == false then
             os.sleep(0.1)
@@ -247,7 +302,7 @@ local function listenCommands()
                     if text == 'clear' then
                         term.clear()
                     end
-                    modem.broadcast(369, text)
+                    modem.send(server_address, port, text)
                     server_answered = false
                 end
             end
